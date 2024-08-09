9b5060992f7302da23ba957b85174822fd6199ae
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-09 20:40:47 +0300

    Implement IProduct interface and Product class

---
+++ Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using Inventory_Management_System.Interfaces;
+
+namespace InventoryManagement.Core.Models
+{
+    public class Product : IProduct
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string Name, decimal Price, int Quantity)
+        {
+            this.Name = Name;
+            this.Price = Price;
+            this.Quantity = Quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Price: {Price}, Quantity: {Quantity}";
+        }
+    }
+}
