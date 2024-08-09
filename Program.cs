8117e8d8a12dea25f1153fa124a122b9fc0e4073
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-09 21:20:09 +0300

    Implement MenuDisplay class to handle menu display

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Inventory_Management_System.Utilities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool exit = false;
+        while (!exit)
+        {
+            MenuDisplay.ShowMenu();
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    MenuDisplay.ShowExitMessage();
+                    exit = true;
+                    break;
+                default:
+                    MenuDisplay.ShowInvalidInputMessage();
+                    Console.ReadKey(); 
+                    break;
+            }
+        }
+    }
+}
