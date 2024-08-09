46db2b0560de596d17b4049f18718da5ee6788bc
mahamdeh0 <mahamdehabd@gmail.com>
2024-08-09 22:15:12 +0300

    Implement InventoryOperations for product management

---
+++ Operations/InventoryOperations .cs
@@ -0,0 +1,25 @@
+﻿using Inventory_Management_System.Interfaces;
+using Inventory_Management_System.Models;
+using InventoryManagement.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Management_System.Operations
+{
+
+    public class InventoryOperations
+    {
+        private readonly Iinventory _inventory;
+
+        public InventoryOperations(Iinventory inventory)
+        {
+            _inventory = inventory;
+        }
+
+
+    }
+}
+
